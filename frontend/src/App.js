ef115c00fcd67d6f5b5c3bcae7b0d24c9494e2c3
jeshmi-hub <rajakjeshmi19@gmail.com>
2023-07-09 19:08:06 +0545

    Ecommerce using mern stack

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+
+const App = () =>{
+  return (
+    <>
+    <Header/>
+    <h1>Welcome to Koba</h1>;
+    </>
+  )
+}
+
+export default App
